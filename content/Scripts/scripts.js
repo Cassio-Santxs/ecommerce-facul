8f1d21fca1be4e69358d2a31d543bd1e3c7f433d
Cássio Vagner dos Santos Diogo <cassiovsd@hotmail.com>
2023-04-13 21:42:38 -0300

    Add files via upload

---
+++ content/Scripts/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var botaoAcessar = document.getElementById("botao")
+
+    botaoAcessar.addEventListener('click', function() {
+        location.href = 'content/View/product.html';
+    });
+});
+  
